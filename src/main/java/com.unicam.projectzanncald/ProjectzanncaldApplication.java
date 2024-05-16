a31746bb977110f40a8570009c179d357aedfe1c
iamShallo <fra.caldarelli02@gmail.com>
2024-05-16 16:02:40 +0200

    springboot

---
+++ src/main/java/com.unicam.projectzanncald/ProjectzanncaldApplication.java
@@ -0,0 +1,13 @@
+package projectzanncald;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectzanncaldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectzanncaldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectzanncald/ProjectzanncaldApplicationTests.java
@@ -0,0 +1,13 @@
+package projectzanncald;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectzanncaldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
