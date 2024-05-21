e4ea3bf8a990e2d881c790c3bed2cd02f58d5035
iamShallo <fra.caldarelli02@gmail.com>
2024-05-21 15:36:16 +0200

    Create ItineraryRepository.java

---
+++ src/main/java/Repository/ItineraryRepository.java
@@ -0,0 +1,19 @@
+package Repository;
+
+import com.unicam.projectzanncald.model.content.Itinerary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItineraryRepository extends JpaRepository<Itinerary,Integer> {
+
+    @Query("SELECT i FROM Itinerary i WHERE i.name = :title")
+    Optional<Itinerary> findByDescription(@Param("title") String title);
+
+    @Query("SELECT i FROM Itinerary i")
+    List<Itinerary> findAllItineraries();
+
+}
