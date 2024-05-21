d8c550c94d57c7d12190fde4e8affd85a3b10a03
iamShallo <fra.caldarelli02@gmail.com>
2024-05-21 15:35:53 +0200

    Create ContestRespository.java

---
+++ src/main/java/Repository/ContestRespository.java
@@ -0,0 +1,11 @@
+package Repository;
+
+import com.unicam.projectzanncald.model.content.Contest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ContestRespository extends JpaRepository<Contest,Integer> {
+    @Query ("SELECT x FROM Contest x WHERE x.name= :title")
+    Contest findAllByTitle(@Param("title") String title);
+}
