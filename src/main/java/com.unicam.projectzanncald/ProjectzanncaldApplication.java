f26275e6973d03a6bc5fc2286a8b4173f37a13dd
iamShallo <fra.caldarelli02@gmail.com>
2024-05-22 15:53:14 +0200

    Update ProjectzanncaldApplication.java

--- src/main/java/com.unicam.projectzanncald/ProjectzanncaldApplication.java
+++ src/main/java/com.unicam.projectzanncald/ProjectzanncaldApplication.java
@@ -1,13 +1,13 @@
-package projectzanncald;
+package com.unicam.projectzanncald;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectzanncaldApplication {
+public class projectzanncaldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectzanncaldApplication.class, args);
+		SpringApplication.run(projectzanncaldApplication.class, args);
 	}
 
 }
