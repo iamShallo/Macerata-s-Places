db8f4543316a5ea45f62be95b53f65f110ef7fcc
iamShallo <fra.caldarelli02@gmail.com>
2024-05-25 15:16:58 +0200

    Create ContestExceptionController.java

---
+++ src/main/java/Exception/ContestExceptionController.java
@@ -0,0 +1,18 @@
+package Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ContestExceptionController {
+    @ExceptionHandler(value=ContestNotExistException.class)
+    public ResponseEntity<Object> exception(ContestNotExistException exception){
+        return new ResponseEntity<>("Contest not exist", HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(value=ContestAlreadyInException.class)
+    public ResponseEntity<Object> exception(ContestAlreadyInException exception){
+        return new ResponseEntity<>("Contest already in!", HttpStatus.BAD_REQUEST);
+    }
+}
