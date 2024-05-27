34c3a2624633ca9666f1b4ac30876ca8e2f2d7a6
SaverioZannotti <saverio.zannotti@studenti.unicam.it>
2024-05-27 17:54:34 +0200

    specificLogin.js

---
+++ src/main/resources/Assets/specificLogin.js
@@ -0,0 +1,25 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita il comportamento predefinito del form
+
+    var email = document.getElementById('email').value.toLowerCase();
+    switch (true){
+        case email.includes('animatore'):
+            window.location.href = '/animatore/animatore.html';
+            break;
+        case email.includes('contributorautorizzato'):
+            window.location.href = 'contributorAutorizzato/contributorAutorizzato.html';
+            break;
+        case email.includes('curatore'):
+            window.location.href = '/curatore/curatore.html';
+            break;
+        case email.includes('turistaautorizzato'):
+            window.location.href = '/turistaAutorizzato/turistaAutorizzato.html';
+            break;
+        case email.includes('gestorepiattaforma'):
+            window.location.href = '/Utenti/gestorePiattaforma/gestorePiattaforma.html';
+            break;
+        default:
+            alert('Effettua il login');
+    }
+});
+
