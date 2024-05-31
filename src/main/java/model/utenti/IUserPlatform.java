043893c3836e3381e0f43550ff6fee23801d6195
iamShallo <fra.caldarelli02@gmail.com>
2024-05-31 15:31:20 +0200

    Create IUserPlatform.java

---
+++ src/main/java/model/utenti/IUserPlatform.java
@@ -0,0 +1,27 @@
+package model.utenti;
+
+import jakarta.persistence.MappedSuperclass;
+
+/**
+ * Interfaccia che rappresenta una piattaforma utente in un sistema.
+ * Definisce i metodi per recuperare le informazioni di base su un utente.
+ */
+@MappedSuperclass
+public interface IUserPlatform {
+
+    int getId();
+
+    UserRole getUserType();
+
+    String getName();
+
+    String getSurname();
+
+    String getUsername();
+
+    String getEmail();
+
+    String getPassword();
+
+    void setRole(UserRole role);
+}
