100ff948d448cc93be5d2390ecd0b0d348831c5c
SaverioZannotti <saverio.zannotti@studenti.unicam.it>
2024-05-31 15:52:10 +0200

    Create AreeVerdi.java

---
+++ src/main/java/punti/AreeVerdi.java
@@ -0,0 +1,30 @@
+package punti;
+
+import com.unicam.idsproject2324.model.content.Point;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+/**
+ * Questa classe rappresenta le aree verdi, ovvero un tipo di punto su una mappa con caratteristiche specifiche, come parchi o giardini.
+ * Estende la classe Point e aggiunge un campo per memorizzare le caratteristiche specifiche delle zone verdi.
+ */
+@Entity
+public class AreeVerdi extends Point {
+    @Column(name="characteristics")
+    private String characteristics;
+    public AreeVerdi(Float x, Float y, String type, String name, String characteristics) {
+        super(x , y, type, name);
+        this.characteristics = characteristics;
+    }
+
+    public AreeVerdi() {
+    }
+    public String getCharacteristics() {
+        return characteristics;
+    }
+
+    @Override
+    public AreeVerdi clone() {
+        return new AreeVerdi(this.getX(), this.getY(), super.getType(),super.getName(), this.getCharacteristics());
+    }
+}
