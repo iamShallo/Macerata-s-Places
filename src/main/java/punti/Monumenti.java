97d83c333732cb2721b977020070bf9efac27460
SaverioZannotti <saverio.zannotti@studenti.unicam.it>
2024-05-31 15:52:34 +0200

    Create Monumenti.java

---
+++ src/main/java/punti/Monumenti.java
@@ -0,0 +1,41 @@
+package punti;
+
+import com.unicam.idsproject2324.model.content.Point;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+import java.util.Date;
+/**
+ * Questa classe rappresenta un monumento, ovvero un tipo di punto su una mappa che indica un sito storico o culturale significativo.
+ * Estende la classe Point e aggiunge campi per memorizzare la data di inaugurazione e una breve storia o descrizione del monumento.
+ */
+@Entity
+public class Monumenti extends Point {
+    @Column(name="inaugurationDate")
+    private Date inaugurationDate;
+    @Column(name="story")
+    private String story;
+
+    public Monumenti(Float x, Float y, String type, String name, String story, Date inaugurationDate) {
+        super(x, y,type , name);
+        this.inaugurationDate = inaugurationDate;
+        this.story = story;
+    }
+
+    public Monumenti() {
+
+    }
+
+    public Date getInaugurationDate() {
+        return inaugurationDate;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    @Override
+    public Monumenti clone() {
+        return new Monumenti(this.getX(), this.getY(),super.getName(), super.getType(), this.getStory(), this.getInaugurationDate());
+    }
+}
