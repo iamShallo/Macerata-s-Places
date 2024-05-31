0d893ef6156701c306483b70b15aeefd9b2d9a79
SaverioZannotti <saverio.zannotti@studenti.unicam.it>
2024-05-31 15:53:04 +0200

    Create Piazza.java

---
+++ src/main/java/punti/Piazza.java
@@ -0,0 +1,32 @@
+package punti;
+
+import com.unicam.idsproject2324.model.content.Point;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+/**
+ * Rappresenta una piazza, che è un tipo di punto su una mappa tipicamente utilizzato come spazio pubblico aperto.
+ * Estende la classe Point e include un campo aggiuntivo per memorizzare informazioni storiche sulla piazza.
+ */
+@Entity
+public class Piazza extends Point {
+    @Column(name="history")
+    private String history;
+
+    public Piazza(Float x, Float y, String type, String name, String history) {
+        super(x , y, type,name);
+        this.history = history;
+    }
+
+    public Piazza() {
+    }
+
+    public String getHistory() {
+        return history;
+    }
+
+    @Override
+    public Piazza clone() {
+        return new Piazza(this.getX(), this.getY(), super.getType(),super.getName(), this.history);
+    }
+}
